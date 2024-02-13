8d5558037669278bb2c94b3b5bc8d7e2d6f1db35
tesnim-mtir <tasnim.mtir@esprim.tn>
2024-02-14 00:02:30 +0100

    inscription

---
+++ resources/views/Home/components/footer.blade.php
@@ -0,0 +1,13 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+
+
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; Copyright <strong><span>CuraVibe</span></strong>. All Rights Reserved
+      </div>
+  
+    </div>
+  </footer><!-- End Footer -->
+  
