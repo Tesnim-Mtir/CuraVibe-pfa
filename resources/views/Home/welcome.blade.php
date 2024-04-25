c44ea3f158d2cc97e25f6d601846fb0a007fd4b7
tesnim-mtir <tasnim.mtir@esprim.tn>
2024-04-25 12:29:01 +0100

    !

--- resources/views/Home/welcome.blade.php
+++ resources/views/Home/welcome.blade.php
@@ -209,7 +209,7 @@ Gestion des stocks de médicaments :
       </div>
     </section><!-- End Cta Section -->
  <!-- ======= Don Section ======= -->
- <section id="don" class="portfolio">
+ {{-- <section id="don" class="portfolio">
       <div class="container" data-aos="fade-up">
 
         <div class="section-title">
@@ -245,7 +245,7 @@ Gestion des stocks de médicaments :
       </div>
 
     </section><!-- End Portfolio Section -->
-
+ --}}
   <!-- ======= Pharmacies Section ======= -->
   <section id="portfolio" class="portfolio">
     <div class="section-title">
@@ -292,79 +292,23 @@ Gestion des stocks de médicaments :
           </div>
         </div>
       </div>
-    </section><!-- End Portfolio Section -->
+    </div>
+  </section> <!-- End Portfolio Section -->
 
+
+ 
     <!-- ======= Contact Section ======= -->
     <section id="contact" class="contact">
       <div class="container" data-aos="fade-up">
 
-        <div class="section-title">
-          <h2>Contact</h2>
-        </div>
-
-        <div class="row">
-
-          <div class="col-lg-5 d-flex align-items-stretch">
-            <div class="info">
-       
-
-              <div class="email">
-                <i class="bi bi-envelope"></i>
-                <h4>Email:</h4>
-                <p>Curavibe@CuraVibe.com</p>
-              </div>
-
-              <div class="phone">
-                <i class="bi bi-phone"></i>
-                <h4>Numéro:</h4>
-                <p>+216 74 512 486</p>
-              </div>
-
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51805.742951773944!2d10.810439599999999!3d35.75427185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x130212b61d744f59%3A0xf284743e1d3e40ef!2sMonastir!5e0!3m2!1sfr!2stn!4v1703721373753!5m2!1sfr!2stn" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
-            </div>
-
-          </div>
-
-          <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
-            @if(session('success'))
-            <div class="alert alert-success">{{ session('success') }}</div>
-        @endif
-        <form action="{{ route('submitForm') }}" role="form" class="php-email-form" method="POST">
-          @csrf
-              <div class="row">
-                <div class="form-group col-md-6">
-                  <label for="name">Votre Nom</label>
-                  <input type="text" name="name" class="form-control" id="name" required>
-                </div>
-                <div class="form-group col-md-6">
-                  <label for="name">Votre Email</label>
-                  <input type="email" class="form-control" name="email" id="email" required>
-                </div>
-              </div>
-              <div class="form-group">
-                <label for="name">Objet</label>
-                <input type="text" class="form-control" name="subject" id="subject" required>
-              </div>
-              <div class="form-group">
-                <label for="name">Message</label>
-                <textarea class="form-control" name="message" rows="10" required></textarea>
-              </div>
-              <div class="my-3">
-                <div class="loading">Chargement</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Votre message a été envoyé. Merci !</div>
-              </div>
-              <div class="text-center"><button type="submit">Envoyer le message</button></div>
-            </form>
-          </div>
-
+        
         </div>
 
       </div>
     </section><!-- End Contact Section -->
 
   </main><!-- End #main -->
-  @include('home.components.footer')
+        @include('Home/components.footer')
   <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DonationController;
+use App\Http\Controllers\CliniqueController;
 use App\Http\Controllers\ChartJSController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\RecycleController;
 
 
 
@@ -26,8 +28,27 @@ Route::get('/', function () {
 Route::get('/inscription', function () {
     return view('home/inscription');
 });
-Route::get('/contact', function () {
-    return view('/contact');
+
+Route::get('/edit', [DonationController::class, 'history'])->name('don.history');
+Route::delete('/edit/{id}', [DonationController::class, 'destroy'])->name('don.destroy');
+
+
+Route::get('/', [DonationController::class, 'home']);
+
+Route::get('/don', [DonationController::class, 'index'])->name('don.index');
+Route::get('/cabinet', [CliniqueController::class, 'index'])->name('cabinet.index');
+
+
+Route::get('/don/{id}', [DonationController::class, 'show'])->name('don_details');
+
+Route::get('/add', [DonationController::class, 'create'])->name('donation.create');
+Route::post('/add', [DonationController::class, 'store'])->name('donation.store');
+Route::post('/don/{id}', 'DonationController@updateStatus')->name('don.updateStatus');
+
+
+
+Route::get('/1', function () {
+    return view('home/don/add');
 });
 
 
@@ -35,7 +56,9 @@ Route::get('/contact', function () {
 Route::get('/Graphique', [ChartJSController::class, 'yourControllerMethod '])->name('graphique');
 
 Route :: get ( '/Graphique' , [ ChartJSController :: class , 'donutChart' ]);
-Route::get('/Graphique', [ChartJSController :: class , 'fetchStatistics'])->name('graphique');
+Route::get('/Graphique', [ChartJSController :: class , 'fetchStatistics'])->name('fetch.statistics');
+
+
 Route::get('/Graphique', [ChartJSController::class, 'showGraphiquePage'])->name('graphique');
 
 Route::get('/graphique', function () {
@@ -44,12 +67,11 @@ Route::get('/graphique', function () {
 Route::get('/statistique', function () {
     return view('/statistique');
 });
-use App\Http\Controllers\RecycleController;
 
 Route::get('/Graphique', [RecycleController::class, 'index'])->name('graphique');
 
-
-
+Route::get('/contact', [ContactController::class, 'showForm'])->name('contact.show');
+Route::post('/contact', [ContactController::class, 'submitForm'])->name('contact.submit');
 
 
 
