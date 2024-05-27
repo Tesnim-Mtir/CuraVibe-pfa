8bc1792e0eb0c3d109e9e10684464fa45805815b
tesnim-mtir <tasnim.mtir@esprim.tn>
2024-05-27 20:43:32 +0100

    :)

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,7 @@ use App\Http\Controllers\CliniqueController;
 use App\Http\Controllers\ChartJSController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\RecycleController;
-
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -73,5 +72,34 @@ Route::post('/contact', [ContactController::class, 'submitForm'])->name('contact
 
 
 
+/*Userspace Routes*/
+
+Route::get('/traitment', [UserController::class, 'newtraitment']);
+
+Route::POST('/traitment/add', [UserController::class, 'addnewtraitment']);
+
+
+Route::get('/traitement/{id}', [UserController::class, 'delete']);
+
+Route::get('/history', [UserController::class, 'history'])->name('history');
+
+Route::get('/allergy', [UserController::class, 'showallergy']);
+//Route::get('/allergys', [UserController::class, 'showallergy_user']);
+Route::post('/allergy/add', [UserController::class, 'addallergy']);
+Route::get('/history/{id}', [UserController::class, 'historydelete']);
+Route::get('/profile', [UserController::class, 'profile']);
+Route::POST('/profile/update', [UserController::class, 'update']);
+//Route::match(['get', 'post'], 'profile/update', [UserController::class, 'update'])->name('profile.update');
+
+Route::get('/ongoing', [UserController::class, 'ongoing'])->name('ongoing');
+Route::get('/register', [UserController::class, 'register']);
+Route::post('/signup', [UserController::class, 'signup']);
+Route::get('/signout', [UserController::class, 'signout']);
+Route::post('/login', [UserController::class, 'login']);
+Route::get('/sendsms', [UserController::class, 'sendsms']);
+Route::get('/mypharmacie', [UserController::class, 'mypharmacie']);
+Route::post('/mypharmacie/add', [UserController::class, 'addtomypharmacie']);
+Route::get('/mypharmacie/updatestock/{id}', [UserController::class, 'updatestock']);
+Route::get('/mypharmacie/delete/{id}', [UserController::class, 'deletefrommypharmacie']);
 
 
